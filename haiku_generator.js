c2517a8dce5d98a812cc99d417c66554ed924b8a
Octowl <alsaff1987@gmail.com>
2016-04-16 01:16:34 -0400

    Fix formatting

--- haiku_generator.js
+++ haiku_generator.js
@@ -3,11 +3,18 @@ var cmuDictFile = process.argv[2];
 var textFile = process.argv[3];
 
 function formatOutput(haiku) {
-  haiku.forEach(function(line) {
-    if (Array.isArray(line)) line = line.join(" ");
-    console.log(line);
-  });
+    console.log("");
+    haiku.forEach(function(line) {
+        if (Array.isArray(line)) line = line.join(" ");
+        console.log(line);
+    });
+    console.log("");
 }
 
-haiku.syllableArraySetup(cmuDictFile, textFile);
-formatOutput(haiku.createHaiku([5,7,5]));
+haiku.setup(cmuDictFile, textFile);
+
+if (textFile) {
+    haiku.findHaikus().forEach(formatOutput);
+} else {
+    formatOutput(haiku.createHaiku());
+}
